329be50b6902073c835330865fb7cbfbabe3af2a
Chimoha31 <mokochii1108@gmail.com>
2022-03-04 13:44:14 -0800

    map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
 
 function App() {
+  const list = [1, 2, 3,4];
+
+  const persons = [
+    {
+      name: "Teddy",
+      age: 1,
+    },
+    {
+      name: "Hana",
+      age: 11,
+    }
+  ]
+
   return (
     <div className="App">
-      This is Test.
+      <h1>Map practice</h1>
+      {list.map((li, index) => (
+        <div key={index}>{li * 2}</div>
+      ))}
+    <div>
+
+      {persons.map((person, i) => (
+        <div key={i}>
+          I'm {person.name}, {person.age} year old
+        </div>
+        ))}
+    </div>
     </div>
   );
 }

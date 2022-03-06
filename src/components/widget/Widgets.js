ad891273145681eaceb63310fefa0f59f7da5ab6
Chimoha31 <mokochii1108@gmail.com>
2022-03-05 17:38:02 -0800

    style for widgets

--- src/components/widget/Widgets.js
+++ src/components/widget/Widgets.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Search } from "@mui/icons-material";
-
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import "./Widgets.css";
 
 function Widgets() {
   return (
@@ -12,9 +17,19 @@ function Widgets() {
 
       <div className="widgets__widgetContainer">
         <h2>Wha you doing now?</h2>
-
         {/* ライブラリを追加 */}
-       
+        <TwitterTweetEmbed tweetId={"987013224703737856"} />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="rihanna" //@usename
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={"https://twitter.com/rihanna"}
+          options={{ text: "#React.js is awesome", via: "chimoha31" }}
+        />
       </div>
     </div>
   );

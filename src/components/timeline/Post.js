1a8305f8106a2baa21927471393b2b7f5c541ba6
Chimoha31 <mokochii1108@gmail.com>
2022-03-06 20:30:47 -0800

    fied bug

--- src/components/timeline/Post.js
+++ src/components/timeline/Post.js
@@ -1,40 +1,46 @@
-import React from 'react';
+import {
+  ChatBubbleOutline,
+  FavoriteBorder,
+  PublishOutlined,
+  Repeat,
+  VerifiedUser,
+} from "@mui/icons-material";
+import { Avatar } from "@mui/material";
+import React, { forwardRef } from "react";
 import "./Post.css";
-import { ChatBubbleOutline, FavoriteBorder, PublishOutlined, Repeat, VerifiedUser } from '@mui/icons-material';
-import { Avatar } from '@mui/material';
 
-function Post({displayName, userName, verified, text, avatar, image}) {
-  return (
-    <div className="post">
-      <div className="post__avatar">
-        <Avatar src={avatar} />
-      </div>
-
-      <div className="post__body">
-        <div className="post__header">
-          <div className="post__headerText">
-            <h3>{displayName}
-            <span className="post__headerSpecial">
-              <VerifiedUser className="post--badge" />
-              @{userName}
-            </span>
-            </h3>
+const Post = forwardRef(
+  ({ displayName, username, verified, text, image, avatar }, ref) => {
+    return (
+      <div className="post" ref={ref}>
+        <div className="post__avatar">
+          <Avatar src={avatar} />
+        </div>
+        <div className="post__body">
+          <div className="post__header">
+            <div className="post__headerText">
+              <h3>
+                {displayName}
+                <span className="post__headerSpecial">
+                  <VerifiedUser className="post--badge" />@{username}
+                </span>
+              </h3>
+            </div>
+            <div className="post__headerDescription">
+              <p>{text}</p>
+            </div>
           </div>
-          <div className="post__headerDescription">
-            <p>{text}</p>
+          <img src={image} alt="" />
+          <div className="post__footer">
+            <ChatBubbleOutline fontSize="small" />
+            <Repeat fontSize="small" />
+            <FavoriteBorder fontSize="small" />
+            <PublishOutlined fontSize="small" />
           </div>
         </div>
-
-        <img src={image} alt="" />
-        <div className="post__footer">
-          <ChatBubbleOutline fontSize="small" />
-          <Repeat fontSize="small" />
-          <FavoriteBorder fontSize="small" />
-          <PublishOutlined fontSize="small" />
-        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
 
 export default Post;
--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -3,7 +3,13 @@ import TweetBox from "./TweetBox";
 import "./Timeline.css";
 import Post from "./Post";
 import db from "../../firebase";
-import { collection, getDocs, onSnapshot, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import FlipMove from "react-flip-move";
 
 function Timeline() {
@@ -30,17 +36,19 @@ function Timeline() {
       {/* TweetBox */}
       <TweetBox />
       {/* Post */}
-      {posts.map((post, index) => (
-        <Post
-          key={index}
-          displayName={post.displayName}
-          userName={post.userName}
-          verified={post.verified}
-          text={post.text}
-          avatar={post.avatar}
-          image={post.image}
-        />
-      ))}
+      <FlipMove>
+        {posts.map((post) => (
+          <Post
+            key={post.text}
+            displayName={post.displayName}
+            userName={post.userName}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
+      </FlipMove>
     </div>
   );
 }
